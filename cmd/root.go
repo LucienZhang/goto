1e427f9229e87d0f1d428ae68bfb059ce596d34c
Lucien <lucienzhangzl@outlook.com>
2021-09-23 17:14:09 +0900

    Init root command

---
+++ cmd/root.go
@@ -0,0 +1,100 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
+
+	"github.com/manifoldco/promptui"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+const CONFIG_FILE_PATH = ".goto/.goto.yaml"
+
+type commandEntity struct {
+	Name  string
+	Desc  string
+	Color string
+	Cmd   string
+}
+
+func (c commandEntity) RGB(v interface{}) string {
+	end := ""
+	s, ok := v.(string)
+	if !ok || !strings.HasSuffix(s, promptui.ResetCode) {
+		end = promptui.ResetCode
+	}
+	return fmt.Sprintf("%s%sm%v%s", "\033[", "38;2;"+c.Color, v, end)
+}
+
+type config struct {
+	Shell    string
+	Commands []commandEntity
+}
+
+var (
+	conf      *config
+	templates = promptui.SelectTemplates{
+		Active:   fmt.Sprintf("%s {{ .Name | underline | .RGB }}", promptui.IconSelect),
+		Inactive: "  {{ .Name | .RGB }}",
+		Selected: fmt.Sprintf(`{{ "%s" | green }} Going to {{ .Name | faint }}`, promptui.IconGood),
+		Details:  "{{ .Desc | faint }}",
+	}
+	rootCmd = &cobra.Command{
+		Use:   "goto",
+		Short: "Goto is an interactive command-line tool to manage your environments",
+		Long: `An interactive command-line tool to manage your environments.
+Complete documentation is available at https://github.com/LucienZhang/goto`,
+		Version: "0.0.1",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			prompt := promptui.Select{
+				Label:     "Select an environment to go",
+				Items:     conf.Commands,
+				Templates: &templates,
+			}
+			idx, _, err := prompt.Run()
+			cobra.CheckErr(err)
+			shellCmd := exec.Command(conf.Shell, "-c", conf.Commands[idx].Cmd)
+			shellCmd.Stdin = os.Stdin
+			shellCmd.Stdout = os.Stdout
+			shellCmd.Stderr = os.Stderr
+			return shellCmd.Run()
+		},
+	}
+)
+
+func Execute() {
+	err := rootCmd.Execute()
+	cobra.CheckErr(err)
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+}
+
+func initConfig() {
+	home, err := os.UserHomeDir()
+	cobra.CheckErr(err)
+	fullConfigFilePath := path.Join(home, CONFIG_FILE_PATH)
+	viper.SetConfigFile(fullConfigFilePath)
+
+	if err := viper.ReadInConfig(); err != nil {
+		if os.IsNotExist(err) {
+			err := os.MkdirAll(path.Dir(fullConfigFilePath), 0755)
+			cobra.CheckErr(err)
+			file, err := os.OpenFile(fullConfigFilePath, os.O_RDONLY|os.O_CREATE, 0644)
+			cobra.CheckErr(err)
+			file.Close()
+		} else {
+			cobra.CheckErr(err)
+		}
+	}
+
+	conf = &config{"bash", []commandEntity{{"Help", "Show help information", "255;255;51", `echo 'Please config your commands in file ~/.goto/.goto.yaml.
+Complete documentation is available at https://github.com/LucienZhang/goto'`}}}
+	err = viper.Unmarshal(conf)
+	cobra.CheckErr(err)
+}
---
+++ go.mod
@@ -0,0 +1,32 @@
+module github.com/LucienZhang/goto
+
+go 1.17
+
+require (
+	github.com/spf13/cobra v1.2.1
+	github.com/spf13/viper v1.8.1
+)
+
+require (
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/juju/ansiterm v0.0.0-20180109212912-720a0952cc2a // indirect
+	github.com/lunixbochs/vtclean v0.0.0-20180621232353-2d01aacdc34a // indirect
+	github.com/magiconair/properties v1.8.5 // indirect
+	github.com/manifoldco/promptui v0.8.0 // indirect
+	github.com/mattn/go-colorable v0.0.9 // indirect
+	github.com/mattn/go-isatty v0.0.4 // indirect
+	github.com/mitchellh/mapstructure v1.4.1 // indirect
+	github.com/pelletier/go-toml v1.9.3 // indirect
+	github.com/spf13/afero v1.6.0 // indirect
+	github.com/spf13/cast v1.3.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+	golang.org/x/text v0.3.5 // indirect
+	gopkg.in/ini.v1 v1.62.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
